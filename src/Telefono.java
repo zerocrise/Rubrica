2ce94b1205244ce1ff4d80ba30990c884b917c46
Succo Rolando <48627654+zerocrise@users.noreply.github.com>
2021-03-05 11:39:46 +0100

    Update Telefono.java

--- src/Telefono.java
+++ src/Telefono.java
@@ -1,23 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
-<<<<<<< HEAD
 
-public class Telefono extends JFrame {
-private JTextField nome,cognome,numero;
-
-public Telefono(){
-=======
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Telefono extends JFrame implements ActionListener {
-private JTextField nome,cognome,numero;
-private  Rubrica rubrica;
-private int indice=0;
+    private JTextField nome,cognome,numero;
+    private  Rubrica rubrica;
+    private int indice=0;
 public Telefono(){
     rubrica=new Rubrica();
-
->>>>>>> Initial commit
 this.setTitle("Rubrica");
 JPanel p1=new JPanel();
 p1.add(new JLabel("Cognome:"));
@@ -31,8 +23,6 @@ numero=new JTextField(12);
 p1.add(numero);
 this.add(p1, BorderLayout.CENTER);
 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-<<<<<<< HEAD
-=======
 JButton aggiungi=new JButton("Add");
 p1.add(aggiungi);
 aggiungi.addActionListener(this);
@@ -45,8 +35,6 @@ p2.add(elimina);
 this.add(p2,BorderLayout.LINE_END);
 modifica.addActionListener(new Ascoltatore(rubrica,indice));
 elimina.addActionListener(new Ascltatore(rubrica,indice));
-
->>>>>>> Initial commit
 this.pack();
 this.setVisible(true);
 
@@ -54,8 +42,7 @@ this.setVisible(true);
     public static void main(String[] args){
     new Telefono();
     }
-<<<<<<< HEAD
-=======
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -65,5 +52,4 @@ this.setVisible(true);
      Contatto c=new Contatto(co,no,num);
      rubrica.aggiungi(c);
      }
->>>>>>> Initial commit
 }
